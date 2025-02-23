e358b7dd4da9489cbf5fab5433367660336d59ed
Mike Introini <michael.introini@gmail.com>
2025-02-23 14:46:13 -0500

    refactored Statistic model

--- pyhole6/__init__.py
+++ pyhole6/__init__.py
@@ -1,6 +1,6 @@
 from .client import Pyhole6
 from .exceptions import Pyhole6Error, AuthenticationError
-from .models import Session, Statistic, BlockingStatus, HostInfo, VersionInfo, HistoryEntry
+from .models import Session, StatsEntry, BlockingStatus, HostInfo, VersionInfo, HistoryEntry
 
 __all__ = ['Pyhole6', 'Pyhole6Error', 'AuthenticationError',
-           'Session', 'Statistic', 'BlockingStatus', 'HostInfo', 'VersionInfo', 'HistoryEntry']
+           'Session', 'StatsEntry', 'BlockingStatus', 'HostInfo', 'VersionInfo', 'HistoryEntry']
